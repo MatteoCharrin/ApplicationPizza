2b2b37b25808df59103b0510383ec373dd29059a
MatteoCharrin <80832537+MatteoCharrin@users.noreply.github.com>
2021-03-30 10:42:56 +0200

    Update BDD.sql

--- BDD.sql
+++ BDD.sql
@@ -2,3 +2,6 @@ SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+CREATE DATABASE `Pizza` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `Pizza`;
